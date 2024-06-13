99e5300e305a122ebb62566f768e09478a706109
Kushagra Gupta <kushagra@rapidinnovation.dev>
2024-06-13 16:55:42 +0530

    fixed more changes

--- src/Authorizable.sol
+++ src/Authorizable.sol
@@ -1,31 +1,56 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
+/**
+ * @title Authorizable
+ * @dev Contract module which provides a basic authorization control mechanism, 
+ * where there is an account (an owner) that can grant exclusive access to 
+ * specific functions to authorised accounts.
+ */
 contract Authorizable is Ownable2Step {
-    error UnAuthorized();
+    error UnAuthorised();
 
-    mapping(address => bool) private authorized;
+    mapping(address => bool) private authorised;
 
-    modifier onlyAuthorized() {
-        if (!authorized[msg.sender] && owner() != msg.sender)
-            revert UnAuthorized();
+    /**
+     * @dev Modifier to make a function callable only by authorised accounts.
+     */
+    modifier onlyAuthorised() {
+        if (!isAuthorised(msg.sender)) {
+            revert UnAuthorised();
+        }
         _;
     }
 
-    function addAuthorized(address _toAdd) public onlyOwner {
+    /**
+     * @dev Adds an address to the list of authorised accounts.
+     * Can only be called by the current owner.
+     * @param _toAdd The address to add to the authorised list.
+     */
+    function addAuthorised(address _toAdd) public onlyOwner {
         require(_toAdd != address(0), "Zero Address inserted");
-        authorized[_toAdd] = true;
+        authorised[_toAdd] = true;
     }
 
-    function removeAuthorized(address _toRemove) public onlyOwner {
-        require(_toRemove != msg.sender, "Owner can not be removed");
-        authorized[_toRemove] = false;
+    /**
+     * @dev Removes an address from the list of authorised accounts.
+     * Can only be called by the current owner.
+     * @param _toRemove The address to remove from the authorised list.
+     */
+    function removeAuthorised(address _toRemove) public onlyOwner {
+        require(_toRemove != msg.sender, "Owner cannot be removed");
+        authorised[_toRemove] = false;
     }
 
+    /**
+     * @dev Checks if an address is authorised.
+     * @param _authAdd The address to check.
+     * @return _isAuth True if the address is authorised, false otherwise.
+     */
     function isAuthorised(address _authAdd) public view returns (bool _isAuth) {
-        _isAuth = (authorized[_authAdd] || owner() == _authAdd);
+        _isAuth = (authorised[_authAdd] || owner() == _authAdd);
     }
 }
